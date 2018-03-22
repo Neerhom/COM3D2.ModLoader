7df0648e50a235c89acc4faa88830eda622dce24
Neerhom <32304258+Neerhom@users.noreply.github.com>
2018-03-22 23:13:08 +0200

    Update Hooks.cs

--- COM3D2.ModLoader.Managed/Hooks.cs
+++ COM3D2.ModLoader.Managed/Hooks.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.IO;
 using UnityEngine;
 
+
 namespace COM3D2.ModLoader.Managed
 {
     public static class Hooks
@@ -30,9 +31,9 @@ namespace COM3D2.ModLoader.Managed
             {
                 string path = array[num9];
                 string fileName = Path.GetFileName(path);
-                if (!(Path.GetExtension(fileName) != ".asset_bg") && !global::GameUty.BgFiles.ContainsKey(fileName))
+                if (!(Path.GetExtension(fileName) != ".asset_bg") && !GameUty.BgFiles.ContainsKey(fileName))
                 {
-                    global::GameUty.BgFiles.Add(fileName, global::GameUty.m_ModFileSystem);
+                    GameUty.BgFiles.Add(fileName, GameUty.m_ModFileSystem);
                 }
             }
         }
@@ -44,23 +45,23 @@ namespace COM3D2.ModLoader.Managed
 
         public static void PhotoBGext()
         {
-            HashSet<int> hashSet = new HashSet<int>();
+           
             string[] bgneilist = new string[] { };
             try
             {
                 bgneilist = GameUty.FileSystemMod.GetList("PhotoBG_NEI", AFileSystemBase.ListType.AllFile);
             }
-            catch { Exception e; }
+            catch (Exception) { }
 
          
             if (bgneilist != null && 0 < bgneilist.Length)
             {
                 for (int j = 0; j < bgneilist.Length; j++)
                 {
-                     if (bgneilist[j].Contains("phot_bg_list") && bgneilist[j].Contains(".nei") && bgneilist[j].Replace("photobg_nei", "") != "phot_bg_list.nei")
+                     if (bgneilist[j].Contains("phot_bg_list") && Path.GetExtension(bgneilist[j]) == ".nei" && Path.GetFileName(bgneilist[j]) != "phot_bg_list.nei")
                     {
-                        string f_strFileName = bgneilist[j];
-                        using (AFileBase aFileBase2 = GameUty.FileSystemMod.FileOpen(f_strFileName))
+                        //string f_strFileName = bgneilist[j];
+                        using (AFileBase aFileBase2 = GameUty.FileSystemMod.FileOpen(bgneilist[j]))
                         {
                             using (CsvParser csvParser = new CsvParser())
                             {
@@ -84,7 +85,7 @@ namespace COM3D2.ModLoader.Managed
                                 }
                                 else
                                 {
-                                    Debug.Log($"Skipping invalid file: Mod/{f_strFileName}");
+                                    Debug.Log($"Skipping invalid file: Mod/{bgneilist[j]}");
                                 }
                             }
                         }
@@ -94,29 +95,29 @@ namespace COM3D2.ModLoader.Managed
 
             }
         }
-
+        
         public static void PhotoBGobjext()
         {
            
-            HashSet<int> hashSet = new HashSet<int>();
+            
             string[] BgObj_enabled = new string[] { };
             try
             {
                 BgObj_enabled = GameUty.FileSystemMod.GetList("PhotoBG_OBJ_NEI", AFileSystemBase.ListType.AllFile);
             }
-            catch
-            { Exception e; }
+            catch (Exception) { }
+           
             if (BgObj_enabled != null && 0 < BgObj_enabled.Length)
             {
                
                 
                 for (int j = 0; j < BgObj_enabled.Length; j++)
                 {
-                      if (BgObj_enabled[j].Contains("phot_bg_object_list") && BgObj_enabled[j].Contains(".nei") && BgObj_enabled[j].Replace("photobg_obj_nei", "") != "phot_bg_object_list.nei")
+                    Debug.Log(BgObj_enabled[j]);
+                      if (BgObj_enabled[j].Contains("phot_bg_object_list") && Path.GetExtension(BgObj_enabled[j]) == ".nei" && Path.GetFileName(BgObj_enabled[j]) != "phot_bg_object_list.nei")
                     {
-                        string filename_full = BgObj_enabled[j];
-
-                        using (AFileBase aFileBase = GameUty.FileSystem.FileOpen(filename_full))
+                       
+                        using (AFileBase aFileBase = GameUty.FileSystemMod.FileOpen(BgObj_enabled[j]))
                         {
                             using (CsvParser csvParser = new CsvParser())
                             {
@@ -140,7 +141,7 @@ namespace COM3D2.ModLoader.Managed
                                 }
                                 else
                                 {
-                                    Debug.Log($"Skipping invalid file: Mod/{filename_full}");
+                                    Debug.Log($"Skipping invalid file: Mod/{BgObj_enabled[j]}");
 
                                 }
 
@@ -150,6 +151,9 @@ namespace COM3D2.ModLoader.Managed
                 }
             }
         }
+
+    
+
 
         public static void PhotBGObj_Instantiate_Ext (PhotoBGObjectData self, ref UnityEngine.Object @object)
         {
@@ -161,28 +165,7 @@ namespace COM3D2.ModLoader.Managed
            
         }
 
-
-        public static void ModPmat(ref string[] list)
-        {
-
-            string[] listmod = new string[] { };
-            try
-            {
-                listmod = GameUty.m_ModFileSystem.GetFileListAtExtension(".pmat");
-            }
-            catch { Exception e; }
-           
-            if (listmod != null && list.Length > 0)
-            {
-                List<string> convertlist = list.ToList();
-               
-               convertlist.AddRange(listmod);
-               list = convertlist.ToArray();
-            }
-              
-        }
-
-        
+ 
 
     }
 }
