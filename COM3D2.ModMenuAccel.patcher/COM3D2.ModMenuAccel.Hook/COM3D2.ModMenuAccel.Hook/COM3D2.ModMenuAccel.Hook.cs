6285a9400ee0e511e43159608fb9a5f3fa9687c6
Neerhom <32304258+Neerhom@users.noreply.github.com>
2018-03-22 23:17:40 +0200

    Add files via upload

---
+++ COM3D2.ModMenuAccel.patcher/COM3D2.ModMenuAccel.Hook/COM3D2.ModMenuAccel.Hook/COM3D2.ModMenuAccel.Hook.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace COM3D2.ModMenuAccel.Hook
+{
+    public class ModmenuAccel
+    {
+
+        // this method is executed afer UpdateFileSystemPath assing arc menu filename list to m_aryMenuFiles  
+        // and stops further exectuion of UpdateFileSystemPath after finisihng
+        public static bool Accel()
+        {
+
+            if (GameUty.m_ModFileSystem != null)
+            {
+
+
+                GameUty.m_aryModOnlysMenuFiles = GameUty.m_ModFileSystem.GetFileListAtExtension(".menu"); //  this is where prefrormance gain is, as this only gets paths of menu files
+                
+                GameUty.m_aryMenuFiles = GameUty.m_aryMenuFiles.Concat(GameUty.m_aryModOnlysMenuFiles).ToArray();
+
+              
+            }
+            // the rest of the code is taken from UpdateFileSystemPath assembly game ver 1.07
+            
+            if (GameUty.rid_menu_dic_.Count == 0)
+            {
+                string[] menuFiles = GameUty.MenuFiles;
+                GameUty.rid_menu_dic_ = new Dictionary<int, string>();
+                for (int num12 = 0; num12 < menuFiles.Length; num12++)
+                {
+                    string fileName2 = Path.GetFileName(menuFiles[num12]);
+                    int hashCode = fileName2.ToLower().GetHashCode();
+                    if (!GameUty.rid_menu_dic_.ContainsKey(hashCode))
+                    {
+                        GameUty.rid_menu_dic_.Add(hashCode, fileName2);
+                    }
+                    else
+                    {
+                        NDebug.Assert(fileName2 == GameUty.rid_menu_dic_[hashCode], string.Concat(new string[]
+                        {
+                    "[",
+                    fileName2,
+                    "]と[",
+                    GameUty.rid_menu_dic_[hashCode],
+                    "]は同じハッシュキーです"
+                        }));
+                    }
+                }
+            }
+            return true;
+        }
+
+    }
+}
---
+++ COM3D2.ModMenuAccel.patcher/COM3D2.ModMenuAccel/COM3D2.ModMenuAccel.Patcher/COM3D2.ModMenuAccel.Patcher.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Mono.Cecil;
+using Mono.Cecil.Inject;
+using Mono.Cecil.Cil;
+using System.Reflection;
+using System.IO;
+
+namespace COM3D2.ModMenuAccel
+{
+    public class ModMenuAccel_Patcher
+    {
+        public static readonly string[] TargetAssemblyNames = { "Assembly-CSharp.dll" };
+        private const string HOOK_NAME = "COM3D2.ModMenuAccel.Hook";
+
+
+
+        public static void Patch(AssemblyDefinition assembly)
+        {
+            
+            string assemblyDir = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            string hookDir = $"{HOOK_NAME}.dll";
+            AssemblyDefinition hookAssembly = AssemblyLoader.LoadAssembly(Path.Combine(assemblyDir, hookDir));
+            TypeDefinition ModmenuAccel = hookAssembly.MainModule.GetType($"{HOOK_NAME}.ModmenuAccel");
+
+
+
+
+            TypeDefinition GameUty = assembly.MainModule.GetType("GameUty");
+            GameUty.ChangeAccess("m_aryMenuFiles", true);
+            GameUty.ChangeAccess("m_aryModOnlysMenuFiles", true);
+            GameUty.ChangeAccess("rid_menu_dic_", true);
+
+            MethodDefinition UpdateFileSystemPath = GameUty.GetMethod("UpdateFileSystemPath");
+
+            MethodDefinition Accel = ModmenuAccel.GetMethod("Accel");
+      
+
+            for (int i = 0; i < UpdateFileSystemPath.Body.Instructions.Count; i++)
+            {
+                if (UpdateFileSystemPath.Body.Instructions[i].OpCode == OpCodes.Stsfld)
+                {
+                    FieldReference target = UpdateFileSystemPath.Body.Instructions[i].Operand as FieldReference;
+                    if (target.Name == "m_aryMenuFiles")
+                    {
+                        UpdateFileSystemPath.InjectWith(Accel, i + 1, flags: InjectFlags.ModifyReturn);
+                        break;
+                    }
+
+                }
+            }
+        }
+    }
+}
