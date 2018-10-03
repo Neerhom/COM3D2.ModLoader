6fcbccf0e8369ec13b0921b8a3a196cd79bfad8c
Neerhom <32304258+Neerhom@users.noreply.github.com>
2018-10-03 21:21:11 +0300

    minor code optimisations

--- COM3D2.ModMenuAccel.patcher/COM3D2.ModMenuAccel.Hook/COM3D2.ModMenuAccel.Hook/COM3D2.ModMenuAccel.Hook.cs
+++ COM3D2.ModMenuAccel.patcher/COM3D2.ModMenuAccel.Hook/COM3D2.ModMenuAccel.Hook/COM3D2.ModMenuAccel.Hook.cs
@@ -1,14 +1,15 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.IO;
+using UnityEngine;
 
 namespace COM3D2.ModMenuAccel.Hook
 {
     public class ModmenuAccel
     {
-
+        
         // this method is executed afer UpdateFileSystemPath assing arc menu filename list to m_aryMenuFiles  
         // and stops further exectuion of UpdateFileSystemPath after finisihng
         public static bool Accel()
@@ -16,34 +17,38 @@ namespace COM3D2.ModMenuAccel.Hook
 
             if (GameUty.m_ModFileSystem != null)
             {
-
-
                 GameUty.m_aryModOnlysMenuFiles = GameUty.m_ModFileSystem.GetFileListAtExtension(".menu"); //  this is where prefrormance gain is, as this only gets paths of menu files
-                
-                GameUty.m_aryMenuFiles = GameUty.m_aryMenuFiles.Concat(GameUty.m_aryModOnlysMenuFiles).ToArray();
 
-              
+                for (int i=0;i< GameUty.m_aryModOnlysMenuFiles.Length; i++ )
+                {
+                
+                  GameUty.m_aryModOnlysMenuFiles[i] = Path.GetFileName(GameUty.m_aryModOnlysMenuFiles[i]);
+                }
+
+                GameUty.m_aryMenuFiles = GameUty.m_aryMenuFiles.Concat(GameUty.m_aryModOnlysMenuFiles).ToArray();
+                
             }
-            // the rest of the code is taken from UpdateFileSystemPath assembly game ver 1.07
-            
+         
+            // the rest of the code is taken from UpdateFileSystemPath assembly game ver 1.07 minus KISS' nosense
+         
             if (GameUty.rid_menu_dic_.Count == 0)
             {
-                string[] menuFiles = GameUty.MenuFiles;
+                
                 GameUty.rid_menu_dic_ = new Dictionary<int, string>();
-                for (int num12 = 0; num12 < menuFiles.Length; num12++)
+                foreach (string str in GameUty.m_aryMenuFiles)
                 {
-                    string fileName2 = Path.GetFileName(menuFiles[num12]);
-                    int hashCode = fileName2.ToLower().GetHashCode();
+                                      
+                    int hashCode = str.GetHashCode();
                     if (!GameUty.rid_menu_dic_.ContainsKey(hashCode))
                     {
-                        GameUty.rid_menu_dic_.Add(hashCode, fileName2);
+                        GameUty.rid_menu_dic_.Add(hashCode, str);
                     }
                     else
                     {
-                        NDebug.Assert(fileName2 == GameUty.rid_menu_dic_[hashCode], string.Concat(new string[]
+                        NDebug.Assert(str == GameUty.rid_menu_dic_[hashCode], string.Concat(new string[]
                         {
                     "[",
-                    fileName2,
+                    str,
                     "]と[",
                     GameUty.rid_menu_dic_[hashCode],
                     "]は同じハッシュキーです"
