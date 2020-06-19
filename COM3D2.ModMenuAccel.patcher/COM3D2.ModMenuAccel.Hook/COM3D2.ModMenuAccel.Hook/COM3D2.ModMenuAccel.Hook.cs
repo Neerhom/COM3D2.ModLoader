ca9088a4b7cb3d2619297c11530810c3ff0cbe7f
Neerhom <32304258+Neerhom@users.noreply.github.com>
2020-06-19 16:42:07 +0300

    updated for 1.48

--- COM3D2.ModMenuAccel.patcher/COM3D2.ModMenuAccel.Hook/COM3D2.ModMenuAccel.Hook/COM3D2.ModMenuAccel.Hook.cs
+++ COM3D2.ModMenuAccel.patcher/COM3D2.ModMenuAccel.Hook/COM3D2.ModMenuAccel.Hook/COM3D2.ModMenuAccel.Hook.cs
@@ -10,54 +10,29 @@ namespace COM3D2.ModMenuAccel.Hook
     public class ModmenuAccel
     {
         
-        // this method is executed afer UpdateFileSystemPath assing arc menu filename list to m_aryMenuFiles  
-        // and stops further exectuion of UpdateFileSystemPath after finisihng
-        public static bool Accel()
+        // executed right before game fetches list of files in mod filesystem
+
+        public static void Accel()
         {
-
-            if (GameUty.m_ModFileSystem != null)
+            // initialize m_ModFileSystem if for some reason it has not been to avoid potential crash
+            // also return as there  is nothing to parse in empty filesystem
+            if (GameUty.m_ModFileSystem == null)
             {
-                GameUty.m_aryModOnlysMenuFiles = GameUty.m_ModFileSystem.GetFileListAtExtension(".menu"); //  this is where prefrormance gain is, as this only gets paths of menu files
-
-                for (int i=0;i< GameUty.m_aryModOnlysMenuFiles.Length; i++ )
-                {
-                
-                  GameUty.m_aryModOnlysMenuFiles[i] = Path.GetFileName(GameUty.m_aryModOnlysMenuFiles[i]);
-                }
-
-                GameUty.m_aryMenuFiles = GameUty.m_aryMenuFiles.Concat(GameUty.m_aryModOnlysMenuFiles).ToArray();
-                
+                GameUty.m_ModFileSystem = new FileSystemWindows();
+                return;
             }
-         
-            // the rest of the code is taken from UpdateFileSystemPath assembly game ver 1.07 minus KISS' nosense
-         
-            if (GameUty.rid_menu_dic_.Count == 0)
+
+
+            GameUty.m_aryModOnlysMenuFiles = GameUty.m_ModFileSystem.GetFileListAtExtension(".menu"); //  this is where prefrormance gain is, as this only gets paths of menu files
+
+            for (int i=0;i< GameUty.m_aryModOnlysMenuFiles.Length; i++ )
             {
                 
-                GameUty.rid_menu_dic_ = new Dictionary<int, string>();
-                foreach (string str in GameUty.m_aryMenuFiles)
-                {
-                                
-                    int hashCode = str.GetHashCode();
-                    
-                    GameUty.rid_menu_dic_[hashCode] = str;
-                    
-                   
-                    
-                   // not sure what was the pont of this log
-                   // as it goes nowhere, so i decided to cut off
-                 //       NDebug.Assert(str == GameUty.rid_menu_dic_[hashCode], string.Concat(new string[]
-                 //       {
-                 //   "[",
-                 //   str,
-                 //   "]と[",
-                 //   GameUty.rid_menu_dic_[hashCode],
-                 //   "]は同じハッシュキーです"
-                 //       }));
-                    
-                }
+                GameUty.m_aryModOnlysMenuFiles[i] = Path.GetFileName(GameUty.m_aryModOnlysMenuFiles[i]);
             }
-            return true;
+                
+      
+
         }
 
     }
